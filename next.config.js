3abf48528bf1079bb8ac70045c24473e25b2df7c
fkazadi2 <fabricekaz@gmail.com>
2026-06-06 13:43:47 +0100

    Add webpack alias for cPanel build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
